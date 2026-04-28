03be641f2ae9725583d970eea3646b8bac4e7aca
NTrang81 <your-email@example.com>
2026-04-28 11:45:55 +0200

    welcome site

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-site-layout>
+
+<h1 class="text-2xl mb-4 font-bold">Contact</h1>
+<p>Mail mij op <a class="underline" href="mailto:nico.deblauwe@ehb.be">nico.deblauwe@ehb.be</a></p>
+
+</x-site-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+ * Public routes
+ */
+Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
+Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
+
+/*
+ * User routes
+ */
+
+
+/*
+ * Admin routes
+ */
